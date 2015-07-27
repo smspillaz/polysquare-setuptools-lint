00d3b77a82b6406606c4abdc5af1b5258b017c3f
Sam Spilsbury <smspillaz@gmail.com>
2015-07-27 12:22:09 +0800

    Install coveralls

---
+++ coverage.py
@@ -0,0 +1,28 @@
+# /coverage.py
+#
+# Submit coverage total for a python project to coveralls
+#
+# See /LICENCE.md for Copyright information
+"""The main setup script to bootstrap and set up a python project."""
+
+import os
+
+from collections import defaultdict
+
+
+def run(cont, util, shell, argv=None):
+    """Submit coverage total to coveralls."""
+    del argv
+
+    with util.Task("""Submitting coverage totals"""):
+        py_ver = defaultdict(lambda: "3.4.1")
+        py_cont = cont.fetch_and_import("setup/project/configure_python.py").run(cont,
+                                                                                 util,
+                                                                                 shell,
+                                                                                 py_ver)
+
+        if os.environ.get("CI", None) is not None:
+            with util.Task("""Uploading to coveralls"""):
+                with py_cont.deactivated(util):
+                    util.execute(cont, util.running_output, "coveralls",)
+
